1fc2d09111b328caf0d1c6107e926a2d34427a9b
almight3 <shrivastavap136@gmail.com>
2022-01-03 14:35:49 +0530

    error handling for unhandaledrejection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app =  express()
 const dotenv = require('dotenv')
-const connectDataBase = require('./controller/dbconnect')
+const connectDataBase = require('./dbconnect/dbconnect')
 const productRoutes = require('./routes/producRoutes')
 const bodyParser = require('body-parser')
 const middlewareErrorHandler = require('./middleware/errorhandler')
@@ -16,6 +16,17 @@ app.use(express.json())
 app.use(productRoutes)
 // error handling for product routes middleware function 
 app.use(middlewareErrorHandler)
+
+
+
+//unhandeled error
+process.on('unhandledRejection',(err)=>{
+console.log(`Error ${err.message}`)
+console.log("shuting down the server due to unhadled error")
+server.close(()=>{
+    process.exit(1)
+}) 
+})
 
 // server up and running  at port 8080
 app.listen(process.env.PORT,()=>{
