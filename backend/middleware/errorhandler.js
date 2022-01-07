eb3a6b7b8929a30f37518e12dede33bcbbe71699
almight3 <shrivastavap136@gmail.com>
2022-01-07 23:55:04 +0530

    error handling for invalid ID and JWT Expires

--- backend/middleware/errorhandler.js
+++ backend/middleware/errorhandler.js
@@ -1,7 +1,34 @@
+const ErrorHandler = require("../utils/errorHandler")
+
 module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500
     err.message = err.message || "internal server error"
 
+   //invalid mongoDb ID
+   if(err.name == "CasteError"){
+       const message = `Resouirce not found. Invalid: ${err.path}`;
+       err = new ErrorHandler(message,400)
+   }
+
+
+    // Mongoose duplicate key error 
+    if(err.code === 11000 ){
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message,400)
+    }
+  // JWT Error 
+  if(err.name === "JsonWebTokenrror" ){
+    const message = `Json Web Token is Invalid , try again `
+    err = new ErrorHandler(message,400)
+}
+  
+// JWT Expire        
+if(err.name === "JsonWebTokenrror" ){
+    const message = `Json Web Token is Expires , try again `
+    err = new ErrorHandler(message,400)
+}
+
+    
     res.status(err.statusCode).json({
         success:false,
         message: err.message,
