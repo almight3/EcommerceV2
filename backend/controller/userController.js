609573fb613b586280512ce1dd009a665a9537b7
almight3 <shrivastavap136@gmail.com>
2022-01-09 14:31:39 +0530

    feature - create controller for user routes and add routes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,9 +1,10 @@
-const ErrorHandler = require('../middleware/errorhandler')
+const ErrorHandler = require('../utils/errorHandler')
 const asyncErrorHandler = require('../middleware/asyncErrorHandler')
 const User = require('../models/user')
 const sendJWTToken = require('../utils/genrateJWT')
 const userRegister = asyncErrorHandler(async(req,res,next)=>{
     const {name,email,password} = req.body;
+    console.log(req.body)
     const user = await User.create({
         name,
         email,
@@ -14,7 +15,7 @@ const userRegister = asyncErrorHandler(async(req,res,next)=>{
         }
     });
 
-  genrateJWTToken(user,201,res)
+  sendJWTToken(user,201,res)
     
 });
 
@@ -44,4 +45,31 @@ const loginUser = asyncErrorHandler(async(req,res,next)=>{
 
 })
 
-module.exports = {userRegister,loginUser}
+//logoutUser
+
+const logoutUser = asyncErrorHandler((req,res,next)=>{
+    res.cookie("token",null,{
+        expires: new Date(Date.now()),
+        httpOnly:true,
+    })
+
+    res.status(200).json({
+        success:true,
+        message:"user loged out "
+    })
+})
+
+// get user details 
+const getUserDetails = asyncErrorHandler(async(req,res)=>{
+   
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success:true,
+        user
+    })
+
+})
+
+
+module.exports = {userRegister,loginUser,logoutUser,getUserDetails}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {userRegister, loginUser,logoutUser} = require('../controller/userController')
+const {userRegister, loginUser,logoutUser,getUserDetails} = require('../controller/userController')
+const {authenticatUser,authorizeUser} = require('../middleware/authenticatUser')
 
 
 // register user
@@ -11,4 +12,14 @@ router.post('/login',loginUser)
 
 //logout User
 router.get('/logout',logoutUser)
+
+
+// get user details
+router.get('/profile',authenticatUser,getUserDetails)
+
+
+
+
+
+
 module.exports = router;
