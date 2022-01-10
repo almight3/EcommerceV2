f6b6548e765ab4a0a5e04f253623bc0aa816bc20
almight3 <shrivastavap136@gmail.com>
2022-01-10 14:09:15 +0530

    feature - add review routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {userRegister, loginUser,logoutUser,getUserDetails} = require('../controller/userController')
+const {userRegister, 
+    loginUser,
+    logoutUser,
+    getUserDetails,
+    changePassword, 
+    getAllUsers,
+    userDetails,
+    updateUserRole,
+    updateUserProfile,
+    deleteUser
+} = require('../controller/userController')
 const {authenticatUser,authorizeUser} = require('../middleware/authenticatUser')
 
 
@@ -13,11 +23,26 @@ router.post('/login',loginUser)
 //logout User
 router.get('/logout',logoutUser)
 
+// get user profile details
+router.get('/user',authenticatUser,getUserDetails)
 
-// get user details
-router.get('/profile',authenticatUser,getUserDetails)
+//  update password
+router.put('/user/password/update',authenticatUser,changePassword)
 
+// update profile
+router.post('user/profile/update',authenticatUser,updateUserProfile)
 
+//get all users
+router.get('/admin/users',authenticatUser,authorizeUser("admin"),getAllUsers)
+
+// get users details admin
+router.get('/admin/users/:id',authenticatUser,authorizeUser('admin'),userDetails)
+
+// update user role
+router.put('/admin/users/:id',authenticatUser,authorizeUser('admin'),updateUserRole)
+
+// delete user
+router.delete('/admin/users/:id',authenticatUser,authorizeUser('admin'),deleteUser)
 
 
 
